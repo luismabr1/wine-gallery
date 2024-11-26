216026e5569eb920c105b1d135b328e1d42da55c
Luis Manuel Brito <lmbritor@pm.com>
2024-11-26 11:07:19 -0400

    main

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import WineGallery from '../components/wine-gallery'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gray-100">
+      <WineGallery />
+    </main>
+  )
+}
+
